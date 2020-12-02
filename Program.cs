5cfb99eb6775dd00fdb477dc4f53d5555504b5d7
Axel Brinck <axel.louis.1991@gmail.com>
2020-12-02 23:17:57 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
